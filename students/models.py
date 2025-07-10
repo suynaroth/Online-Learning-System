857df15d0f192f465b8b847f735971c2c736f37a
suynaroth <suynnaroth@gmail.com>
2025-07-11 00:29:24 +0700

    add phn num and address to sttudent model

--- students/models.py
+++ students/models.py
@@ -5,8 +5,10 @@ class Student(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='student_profile')
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
     date_of_birth = models.DateField()
+
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
