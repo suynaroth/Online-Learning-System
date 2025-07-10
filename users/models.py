0819ecb6df441e7819d486b0737340413980eedd
suynaroth <suynnaroth@gmail.com>
2025-07-11 00:34:27 +0700

    hash password in user moodel

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
 
 
 class User(AbstractUser):
@@ -9,6 +10,11 @@ class User(AbstractUser):
         ('employee', 'Employee'),
     ]
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def save (self, *args, **kwargs):
+        if not self.password.startswith('pbkdf2_sha256$'):
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
 
     def __str__(self):
        return f"{self.username} ({self.role})"
