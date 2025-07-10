a33aa3c8c6b1962e09a4ad5028a7d8d36e933a5b
suynaroth <suynnaroth@gmail.com>
2025-07-10 22:35:44 +0700

    Create Insttructor model

---
+++ instructors/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+class Instructor(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='instructor_profile')
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    expertise = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    class Meta: 
+        ordering = ['last_name', 'first_name']
+
