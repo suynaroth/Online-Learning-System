9135674ba32577f8de0d22c1fe58604f0edd4fbc
suynaroth <suynnaroth@gmail.com>
2025-07-11 00:41:30 +0700

    edit empployee model field

--- employees/models.py
+++ employees/models.py
@@ -5,7 +5,8 @@ class Employee(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='employee_profile')
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
     position = models.CharField(max_length=50)
 
     def __str__(self):
