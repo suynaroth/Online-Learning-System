5109cef959c1ffabd33657085ef15bdccf6108c9
suynaroth <suynnaroth@gmail.com>
2025-07-11 21:43:50 +0700

    update delete form

--- students/urls.py
+++ students/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.student_list, name='student_list'),
     path('registration/', views.student_registration, name='student_registration'),
     path('edit/<int:pk>/', views.edit_student, name='edit_student'),
+    path('delete/<int:pk>/', views.delete_student, name='delete_student'),
 ]
