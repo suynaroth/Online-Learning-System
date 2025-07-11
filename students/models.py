17b49d9602a8fe27db062f1cc690a7e3d7339dcb
suynaroth <suynnaroth@gmail.com>
2025-07-11 12:38:11 +0700

    implement student view

--- students/models.py
+++ students/models.py
@@ -9,7 +9,6 @@ class Student(models.Model):
     address = models.TextField(blank=True, null=True)
     date_of_birth = models.DateField()
 
-
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
--- students/views.py
+++ students/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .models import Student
 from users.models import User
 from .forms import StudentForm
+from users.forms import UserForm
 
 def student_list(request):
     students = Student.objects.select_related('user').all()
@@ -9,24 +10,19 @@ def student_list(request):
 
 def student_registration(request):
     if request.method == 'POST':
-        form = StudentForm(request.POST or None)
-        username = request.POST.get('username')
-        password = request.POST.get()
+        student_form = StudentForm(request.POST or None)
+        user_form = UserForm(request.POST or None)
         
-        if form.is_valid() and username and password:
-            #create user
-            user = User.objects.create_user(
-                username=username,
-                password=password,
-                role = 'student'
-            )
+        if student_form.is_valid() and user_form.is_valid():
+            #create user and student
+            student = student_form.save(commit=False)  #not record the data to database just save in memeory for temp
+            user = user_form.save()
 
-            #creat student and link to user
-            student = form.save(commit=False) #use commit = false to not record the data to database just save in memeory
+            #link student to user
             student.user = user
             student.save()
             return redirect('student_list')
-        return render(request,'students/student_form.html' , {'form' : form})
+        return render(request,'students/student_form.html' , {'student_form' : student_form, 'user_form': user_form})
 
 def edit_student(request, pk):
     student = Student.objects.filter(pk=pk).first()
