696e8f84dbd918d5fc4eacae478da6c124cc31b4
suynaroth <suynnaroth@gmail.com>
2025-07-11 14:07:28 +0700

    craete template

--- users/forms.py
+++ users/forms.py
@@ -4,5 +4,4 @@ from .models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = [' username',' password']
-        widgets = {'password' : forms.PasswordInput(),}
+        fields = ['username', 'password']
