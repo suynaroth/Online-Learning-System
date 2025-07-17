00af51076d5914355ed92796dbd41d57d797d38f
suynaroth <suynnaroth@gmail.com>
2025-07-17 09:56:13 +0700

    add message method

--- enrollments/views.py
+++ enrollments/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages  
 from courses.models import Course
 from .models import Enrollment
 
@@ -7,6 +8,7 @@ def enroll(request, course_id):
     if request.method == 'POST':
         enrollment, created = Enrollment.objects.get_or_create(student=request.user.student, course=course)
         if created:
+            messages.success(request, f"You are enrolled in {enrollment.course.title}")
             return redirect('enrollment_success', course_id=course.id)
         else:
             return redirect('already_enrolled', course_id=course.id)
