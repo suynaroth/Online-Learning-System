1ece7c2d7d7dd048ae50b1fb1ecc8445a52b4edd
suynaroth <suynnaroth@gmail.com>
2025-07-18 10:33:12 +0700

    added templates folder for base UI

--- online_learning_system/settings.py
+++ online_learning_system/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'employees',
     'courses',
     'enrollments',
+    'dashboards'
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'online_learning_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
