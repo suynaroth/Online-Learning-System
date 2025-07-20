4c3920fe025f5206d3c959f514daa25eee76ba2f
suynaroth <suynnaroth@gmail.com>
2025-07-20 14:29:59 +0700

    implement ui

--- online_learning_system/urls.py
+++ online_learning_system/urls.py
@@ -3,10 +3,13 @@ from django.urls import path,include
 from students import urls as student_url
 from instructors import urls as instructor_url
 from courses import urls as course_url
+from django.contrib.auth import views as auth_views
+from dashboards.forms import LoginForm
 
 urlpatterns = [
+    path('', auth_views.LoginView.as_view(authentication_form=LoginForm), name='login'),
     path('admin/', admin.site.urls),
-    path("students/",include(student_url)),
+    path("students/",include('students.urls')),
     path("instructors/",include(instructor_url)),
     path("courses/",include(course_url)),
     path('dashboard/', include('dashboards.urls')),
